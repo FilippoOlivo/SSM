5478b8826e3ee52cecf3975ec11b04b03920dfac
FilippoOlivo <filippo@filippoolivo.com>
2025-04-08 12:22:15 +0200

    Fix compute_hippo

--- ssm/utils.py
+++ ssm/utils.py
@@ -9,35 +9,14 @@ def compute_hippo(N):
     :return: A (N, N) matrix initialized using the HIPPO method.
     :rtype: torch.Tensor
     """
-    P = torch.sqrt(torch.arange(1, 2 * N, 2))
-    A = 0.5 * torch.outer(P, P)
-    A = torch.tril(A, diagonal=-1)
-    diag_indices = torch.arange(N) + 1
-    A = A - torch.diag(diag_indices)
-    return -A
-
-
-def compute_hippo(N):
-    """
-    Constructs the HIPPO hidden-to-hidden matrix A.
-
-    :param int N: The size of the HIPPO matrix.
-    :return: A (N, N) matrix initialized using the HIPPO method
-    :rtype: torch.Tensor
-    """
-    P = torch.sqrt(
-        torch.arange(
-            1,
-            2 * N,
-            2,
-        )
-    )
-    A = 0.5 * (
-        P[:, None] * P[None, :]
-    )  # Outer product (equivalent to broadcasting)
-    A = torch.tril(A, diagonal=-1)
-    A = A + torch.diag(torch.arange(1, N + 1))
-
+    A = torch.zeros(N,N)
+    for n in range(N):
+        tmp = (2*n + 1) ** (1/2)
+        for k in range(n+1):
+            if k == n:
+                A[n,k] = n+1
+            else:
+                A[n,k] = tmp * (2 * k + 1) ** (1/2)
     return -A
 
 
