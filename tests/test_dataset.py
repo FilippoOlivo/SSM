2962aa646bb08ada09169b2ac02e1e67a4522ac9
FilippoOlivo <filippo@filippoolivo.com>
2025-04-15 15:28:01 +0200

    Fix tests

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -32,21 +32,15 @@ def test_generate_data(selective, mem_tokens, sequence_len):
     data = dataset.generate_data()
     assert data is not None
     assert len(data) == 2
-    assert data[0].shape == (32, sequence_len + mem_tokens)
-    assert data[1].shape == (32, sequence_len + mem_tokens)
-    assert torch.isclose(
-        data[1][:, :sequence_len],
-        torch.ones((32, sequence_len), dtype=torch.int64) * -1,
-    ).all()
-    assert [
-        True if i > 0 and i < 4 else False for i in data[0].flatten()
-    ].count(True) == 32 * mem_tokens
-    assert (
-        not all(
-            [
-                True if i > 0 and i < 4 else False
-                for i in data[0][:, :mem_tokens].flatten()
-            ]
-        )
-        == selective
-    )
+
+    input_, target = data
+    expected_input_shape = (32, sequence_len + mem_tokens * 2)
+    expected_target_shape = (32, mem_tokens)
+    assert input_.shape == expected_input_shape
+    assert target.shape == expected_target_shape
+    
+    mask = (input_ > 0) & (input_ < 4)
+    assert mask.sum() == 32 * mem_tokens
+
+    mask = (input_[:, :mem_tokens] > 0) & (input_[:, :mem_tokens] < 4)
+    assert not (mask.all().item() == selective)
