cc54ae9330530f0c78d8ad8d84b843686924d88b
giovanni <giovanni.canali98@yahoo.it>
2025-05-19 11:16:10 +0200

    modify transformer

--- ssm/model/transformer.py
+++ ssm/model/transformer.py
@@ -3,7 +3,7 @@ import torch
 
 class Transformer(torch.nn.Module):
     """
-    Wrapper class for torch.nn.Transformer.
+    Wrapper class for torch.nn.TransformerEncoder.
     """
 
     def __init__(
@@ -11,8 +11,7 @@ class Transformer(torch.nn.Module):
         model_dim,
         hidden_dim,
         heads,
-        num_encoder_layers=2,
-        num_decoder_layers=2,
+        n_layers=2,
         dropout=0.1,
         activation="gelu",
         **kwargs,
@@ -23,8 +22,7 @@ class Transformer(torch.nn.Module):
         :param int model_dim: The input dimension.
         :param int hidden_dim: The hidden state dimension.
         :param int heads: The number of attention heads.
-        :param int num_encoder_layers: Number of encoder layers. Default is 2.
-        :param int num_decoder_layers: Number of decoder layers. Default is 2.
+        :param int n_layers: Number of encoder layers. Default is 2.
         :param float dropout: Dropout rate. Default is 0.1.
         :param str activation: The activation function. Must be one of
             `'gelu'` or `'relu'`. Default is `'gelu'`.
@@ -40,27 +38,28 @@ class Transformer(torch.nn.Module):
                 "The number of heads must be a divisor of the input dimension."
             )
 
-        # Initialize the transformer model
-        self.transformer = torch.nn.Transformer(
+        # Initialize the transformer encoder layer
+        self.encoder_layer = torch.nn.TransformerEncoderLayer(
             d_model=model_dim,
             nhead=heads,
-            num_encoder_layers=num_encoder_layers,
-            num_decoder_layers=num_decoder_layers,
             dim_feedforward=hidden_dim,
             dropout=dropout,
             activation=activation,
             batch_first=True,
         )
 
-    def forward(self, x, y, src_mask=None, tgt_mask=None):
+        self.transformer_encoder = torch.nn.TransformerEncoder(
+            encoder_layer=self.encoder_layer,
+            num_layers=n_layers,
+            norm=torch.nn.LayerNorm(model_dim),
+        )
+
+    def forward(self, x):
         """
         Forward pass through the Transformer model with optional masks.
 
         :param torch.Tensor x: Input tensor (batch, seq length, dimension).
-        :param torch.Tensor y: Target tensor (batch, seq length, dimension).
-        :param torch.Tensor src_mask: Source mask. Default is `None`.
-        :param torch.Tensor tgt_mask: Target mask. Default is `None`.
         :return: Output tensor.
         :rtype: torch.Tensor
         """
-        return self.transformer(x, y, src_mask=src_mask, tgt_mask=tgt_mask)
+        return self.transformer_encoder(x)
